089cea9fd10f781a5a63aeb621e382353d7565c6
mo4you <kkmkm@nate.com>
2019-01-19 18:16:25 +0900

    33

--- fun.js
+++ fun.js
@@ -2,17 +2,18 @@
 
 
   function resizeIF (){
-        var objBody = main.document.body;
-        var objFrame = document.all["main"];
-        objFrame.style.height = "0px";
-        ifrmHeight = objBody.scrollHeight;
-        objFrame.style.height = ifrmHeight;
-        return;
+    var objBody = main.document.body;
+    var objFrame = document.all["main"];
+    objFrame.style.height = "0px";
+    ifrmHeight = objBody.scrollHeight;
+    objFrame.style.height = ifrmHeight;
+    return;
   }
 
   function resizeIF2 (){
         var objBody = main.document.body;
         var objFrame = document.all["main"];
+        
         ifrmHeight = objBody.scrollHeight;
         objFrame.style.height = ifrmHeight;
         return;
