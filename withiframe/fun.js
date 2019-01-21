4a14de2831df29c299ae310c6cff10c6d3c72e84
mo4you <kkmkm@nate.com>
2019-01-21 15:43:00 +0900

    37

---
+++ withiframe/fun.js
@@ -0,0 +1,20 @@
+
+
+
+  function resizeIF (){
+    var objBody = main.document.body;
+    var objFrame = document.all["main"];
+    objFrame.style.height = "0px";
+    ifrmHeight = objBody.scrollHeight;
+    objFrame.style.height = ifrmHeight;
+    return;
+  }
+
+  function resizeIF2 (){
+        var objBody = main.document.body;
+        var objFrame = document.all["main"];
+        
+        ifrmHeight = objBody.scrollHeight;
+        objFrame.style.height = ifrmHeight;
+        return;
+  }
